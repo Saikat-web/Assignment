d9ba8133d2be54c626dcff3d0ac174b9282bd052
unknown <Saikat-web>
2024-07-08 01:32:39 +0600

    first commit

---
+++ largest_number.php
@@ -0,0 +1,14 @@
+<<?php
+$num1 = 4;
+$num2 = 5;
+$num3 = 6;
+
+if ($num1 > $num2 && $num1 > $num2){
+    echo "Largest Number is {$num1}";
+}
+if ($num2 > $num1 && $num2 > $num3){
+    echo "Largest Number is {$num2}";
+}
+if ($num3 > $num1 && $num3 > $num2){
+    echo "Largest Number is {$num3}";
+}
